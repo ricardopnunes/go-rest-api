3e2fcc6e8afb66b630a1440764b71929fc7a3a57
ricardopnunes <ricardopontin145@gmail.com>
2022-08-25 21:16:55 -0300

    Finalizando API de Personalidades

--- main.go
+++ main.go
@@ -4,16 +4,10 @@ import (
 	"fmt"
 
 	"github.com/ricardopnunes/go-rest-api/database"
-	"github.com/ricardopnunes/go-rest-api/models"
 	"github.com/ricardopnunes/go-rest-api/routes"
 )
 
 func main() {
-	models.Personalidades = []models.Personalidade{
-		{Id: 1, Nome: "Nome 1", Historia: "Historia 1"},
-		{Id: 2, Nome: "Nome 2", Historia: "Historia 2"},
-	}
-
 	database.ConectaComDB()
 	fmt.Println("Iniciando o Servidor REST!")
 	routes.HandleRequest()
